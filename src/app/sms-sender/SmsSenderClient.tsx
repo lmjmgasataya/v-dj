7a870985fc99b5611fe0ae7c9132c84722d4f661
J Matthew Gasataya <jmatthew.gasataya@legalmatch.com>
2026-06-26 15:39:35 +0800

    refactor: color and size of resend icon

--- src/app/sms-sender/SmsSenderClient.tsx
+++ src/app/sms-sender/SmsSenderClient.tsx
@@ -481,9 +481,9 @@ export function SmsSenderClient({ batches, templates }: { batches: Batch[]; temp
                     <button
                       onClick={() => resendOne(r.id)}
                       title="Resend"
-                      className="shrink-0 text-gray-400 hover:text-[#00428E] transition"
+                      className="shrink-0 text-green-500 hover:text-green-600 transition"
                     >
-                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-3.5 h-3.5">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-5 h-5">
                         <path fillRule="evenodd" d="M13.836 2.477a.75.75 0 0 1 .75.75v3.182a.75.75 0 0 1-.75.75h-3.182a.75.75 0 0 1 0-1.5h1.37l-.84-.841a4.5 4.5 0 0 0-7.08.932.75.75 0 0 1-1.3-.75 6 6 0 0 1 9.44-1.242l.842.84V3.227a.75.75 0 0 1 .75-.75Zm-.911 7.5A.75.75 0 0 1 13.199 11a6 6 0 0 1-9.44 1.241l-.84-.84v1.371a.75.75 0 0 1-1.5 0V9.591a.75.75 0 0 1 .75-.75H5.35a.75.75 0 0 1 0 1.5H3.98l.841.841a4.5 4.5 0 0 0 7.08-.932.75.75 0 0 1 1.025-.273Z" clipRule="evenodd" />
                       </svg>
                     </button>
